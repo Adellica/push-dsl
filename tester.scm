6e49d53addac1cb8389c49dae4367a25dac7979f
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-08-09 01:18:53 +0200

    test cleanup: use schush.ss
    use original schush.ss from Lee Spector as benchmark comparison. my port
    to Chicken isn't really interesting (it's also a lot slower, for some
    reason ...)
    
    this means we don't get a speedup printout, but we can look at the
    timings of each. I'm getting 2400ms for mzscheme and 145ms for the
    C-generated VM. it's not that big of an improvement, actually. 10x
    faster isn't worth the trouble of losing the flexibility of the Lisp
    languages. and the C runtime will grow too. we need a GC and things like
    that so this is an ambition project.

--- tester.scm
+++ tester.scm
@@ -1,58 +1,52 @@
-(use schush matchable fmt fmt-c clojurian-syntax)
+(use matchable fmt fmt-c clojurian-syntax)
 
-(define max-ticks 10000)
-;; shame on you for copy-pasting, Krish!
-(define (sconc . args) (string->symbol (fold conc "" (reverse args))))(define (identifier op) (->> `(("_" . "__") ("else" . "_else") ("." . "_") ("+" . "_plus_") ("-" . "_minus_") ("*" . "_star_") ("/" . "_divide_") (">" . "_gt_") ("<" . "_lt_") ("%" . "_percent_")) (string-translate* (conc op)) (string->symbol))) (define (op->identifier op) (sconc "OP_" (string-upcase (symbol->string (identifier op)))))
+(define max-ticks 100000000)
+
+;; match the generated identifiers from the source files.
+;; TODO: don't just copy-paste this!
+(define (sconc . args) (string->symbol (fold conc "" (reverse args))))
+(define (identifier op) (->> `(("_" . "__") ("else" . "_else") ("." . "_") ("+" . "_plus_") ("-" . "_minus_") ("*" . "_star_") ("/" . "_divide_") (">" . "_gt_") ("<" . "_lt_") ("%" . "_percent_")) (string-translate* (conc op)) (string->symbol)))
+(define (op->identifier op) (sconc "OP_" (string-upcase (symbol->string (identifier op)))))
 
 
+;; (define program
+;;   (random-code
+;;     40
+;;    `(
+;;      #f #t 0 1 2 3 4 5 6 7 8 9
+;;      boolean.and boolean.frominteger boolean.not boolean.or
+;;      exec.do*count exec.do*range exec.do*times exec.if integer.%
+;;      integer.* integer.+ ;; integer.-
+;;      integer./ integer.< integer.> integer.dup
+;;      integer.fromboolean integer.max integer.min integer.pop integer.rot)))
 
-(define program
-  (random-code
-    1000
-   `(
-     #f #t 0 1 2 3 4 5 6 7 8 9
-     boolean.and boolean.frominteger boolean.not boolean.or
-     exec.do*count exec.do*range exec.do*times exec.if integer.%
-     integer.* integer.+ ;; integer.-
-     integer./ integer.< integer.> integer.dup
-     integer.fromboolean integer.max integer.min integer.pop integer.rot)))
+(define program '(1 1000000 exec.do*times (1 integer.+)))
 
-;;(define program '(1000000 exec.do*times (1 2 integer.> exec.if 100 200 integer.pop)))
 
-(define (print-state state)
-  (print "integer: " (get-stack  'integer state))
-  (print "float:   " (get-stack  'float state))
-  (print "boolean: " (get-stack  'boolean state))
-  (print "exec:    " (get-stack  'exec state))
-  (print "code:    " (get-stack  'code state))
-  (print "ticks:   " (hash-ref   state '_ticks (lambda () #f))))
+(system (conc "mzscheme schush.ss '" program "'"))
 
-(define state (make-schush-state))
+;; (define program '(1.0 30000 exec.do*times (1.0001 float.*) integer.fromfloat))
 
-(print "doing program: " program)
-(define start (current-milliseconds))
-(run-schush program state tick-limit: max-ticks)
-(define elap (- (current-milliseconds) start))
+;; (define program '((integer.< (exec.if (integer.rot) ((integer.+) (exec.do*range (3 (2))) (integer./ (integer.dup)) 7) (exec.do*range)) (exec.do*count))))
 
+;; rewrite an object construct to C code
 (define (rew body)
   (match body
     ((head rest ...)
      `(m_obj_cons ,(rew head) ,(rew rest)))
     (()
      `(& the_empty_list))
-    ((? number? n)
+    ((? fixnum? n)
      `(m_obj_from_integer ,n))
+    ((? flonum? n)
+     `(m_obj_from_float ,n))
     ((? symbol? op)
      `(m_obj_from_op ,(op->identifier op)))
     ((and (or #f #t) x)
      `(m_obj_from_boolean ,(if x 1 0)))
     (else (error "cant mach" body))))
 
-(define (cconc . args)
-  (conc "\033[31m" (apply conc args) "\033[0m"))
-
-(define steps 20)
-
+;; every written C in Lisp before? It's not great ...
 (define main
   `(%begin
 
@@ -84,31 +78,16 @@
           (while (&& (m_apply cpu) (< ticks ,max-ticks))
             (++ ticks))
 
-          ;;(printf "boolean: (") (m_boolean_print cpu)(printf ")\n")
-          ;;(printf "exec:    (") (m_exec_print cpu)   (printf ")\n")
-          
-          ;; (printf "integer: (") (m_integer_print cpu) (printf "\n")
-          ;; (printf "boolean: (") (m_boolean_print cpu)(printf "\n")
-          ;; (printf "exec:    (") (m_exec_print cpu)(printf "\n")
-          ;; (printf "\n")
-          
           (clock_gettime CLOCK_MONOTONIC (& end))
           (= elap (timespecDiff (& end) (& start)))
 
           (printf "integer: (") (m_integer_print cpu)(printf ")\n")
           (printf "boolean: (") (m_boolean_print cpu)(printf ")\n")
-          (printf "  exec:    (") (m_exec_print cpu)   (printf ")\n")
+          (printf "exec:    (") (m_exec_print cpu)   (printf ")\n")
+          (printf "\n")
           (printf "ticks:   %d\n" ticks)
           (printf "elapsed: %.1fms\n" elap)
           (printf "\n")
-
-          (printf "=== expected results:\n")
-          (printf ,(cconc "integer: " (get-stack 'integer state) "\n"))
-          (printf ,(cconc "boolean: " (get-stack 'boolean state) "\n"))
-          (printf ,(cconc "exec:    " (get-stack 'exec state) "\n"))
-          (printf ,(cconc "ticks:   " (hash-ref state '_ticks) "\n"))
-          (printf ,(cconc "elapsed: " elap "ms\n"))
-          (printf "speedup is roughly %.0fx\n" (/ ,elap elap))
 
           (m_machine_free cpu)
           0)))
