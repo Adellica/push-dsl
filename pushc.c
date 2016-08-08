e74e735dd17c9ff62f67f693dc8206ed794d524b
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-08-08 23:39:17 +0200

    cleanup minor: remove silly test-code

--- pushc.c
+++ pushc.c
@@ -413,20 +413,3 @@ int m_apply (m_machine_t *m) {
 }
 
 
-// example program skeleton
-int main_test() {
-  m_machine_t _cpu, *cpu = &_cpu;
-  m_machine_init(cpu);
-
-  int ticks = 0;
-  while (m_apply(cpu)) { ticks++; }
-
-  printf("end at %d ticks\n", ticks);
-  printf("ints:   ");       m_integer_print(cpu);
-  printf("booleans:   ");   m_boolean_print(cpu);
-  printf("exec:   ");       m_exec_print(cpu);
-  printf("\n");
-
-  m_machine_free(cpu);
-  return 0;
-}
